27ae08a3eefc074dfe226a5ff0ddb2ce3a17c7b5
AmeNawa <xAmeNawa@gmail.com>
2016-09-04 13:27:32 +0200

    Rozwiązanie zadań, Training1 (stringOperations i myList)

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/MichałMuzyczka/MichalMyList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornetsTraining.Training1.HomeWork.MichałMuzyczka
+{
+    public class MichalMyList<T>
+    {
+        protected T[] ListOfItems;
+        private int _index = 0;
+        private int _lastIndex = 0;
+
+        public virtual void Add(T item)
+        {
+            var copyList = ListOfItems;
+
+            try
+            {
+                if (ListOfItems == null)
+                    ListOfItems = new T[1];
+                _index++;
+                T[] newList = new T[_index];
+                for (int i = 0; i < _lastIndex; i++)
+                {
+                    newList[i] = this.ListOfItems[i];
+                }
+                newList[_index - 1] = item;
+                _lastIndex++;
+                ListOfItems = newList;
+            }
+            catch
+            {
+                ListOfItems = copyList;
+            }
+        }
+    }
+}
