09e4d858a950da47daecfd9f29d22a792fea7e5c
CINEK\Marcin <ortofone24@gmail.com>
2016-09-05 19:32:16 +0200

    :)

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/BartekZapart/BartekZapartStringOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/BartekZapart/BartekZapartStringOperations.cs
@@ -44,5 +44,10 @@ namespace SeeSharpBasics.BartekZapart
         {
             throw new NotImplementedException();
         }
+
+        public void MarcinGala(string test)
+        {
+            
+        }
     }
 }
