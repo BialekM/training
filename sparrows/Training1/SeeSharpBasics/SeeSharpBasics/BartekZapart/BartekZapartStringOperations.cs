1f151273138c882d277ea36b0a5ffe476d5df56a
Haski <gamil007@gmail.com>
2016-09-05 19:55:34 +0200

    nick + zmiana formatowania tesktu

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/BartekZapart/BartekZapartStringOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/BartekZapart/BartekZapartStringOperations.cs
@@ -4,69 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SeeSharpBasics.BartekZapart
-{
-    public class BartekZapartStringOperations : StringOperations
-    {
-        public override string Substring(string candidate, int start, int length)
-        {
+namespace SeeSharpBasics.BartekZapart {
+    public class BartekZapartStringOperations : StringOperations {
+        public override string Substring(string candidate, int start, int length) {
             //return base.Substring(candidate, start, length);
             // wasze cialo metody
             return "";
         }
 
-        public override string GetName()
-        {
+        public override string GetName() {
             return "Bartek Zapart";
         }
 
-        public override string GlueBeginingEnd(string candidate)
-        {
+        public override string GlueBeginingEnd(string candidate) {
             throw new NotImplementedException();
         }
 
-        public override int CountOccurences(string candidate, char needle)
-        {
+        public override int CountOccurences(string candidate, char needle) {
             throw new NotImplementedException();
         }
 
-        public override int LetterPositionInString(string candidate, char needle)
-        {
+        public override int LetterPositionInString(string candidate, char needle) {
             throw new NotImplementedException();
         }
 
-        public override string LetterReplace(string candidate, char needle, char replace)
-        {
+        public override string LetterReplace(string candidate, char needle, char replace) {
             throw new NotImplementedException();
         }
 
-        public override string StringReplace(string candidate, string needle, string replace)
-        {
+        public override string StringReplace(string candidate, string needle, string replace) {
             throw new NotImplementedException();
         }
 
-        public void AdrianPankiewicz(string test)
-        {
+        public void AdrianPankiewicz(string test) {
         }
 
-        public void RafalKawalerski(string test)
-        {
-            
+        public void RafalKawalerski(string test) {
+
         }
 
-        public void KrzysztofPater(string name)
-        {
-            
+        public void KrzysztofPater(string name) {
+
         }
 
-        public void BartekZapart(string test)
-        {
-            
+        public void BartekZapart(string test) {
+
         }
 
-        public void DominikRzepnicki(string test)
-        {
+        public void DominikRzepnicki(string test) {
             //change
+        }
+
+        public void Huski(string test) {
+            //popsulem formatowanie
         }
     }
 }
