0d1fae59ca88a9c187129491b821d2100e113817
ar-kay <kawalerski.r@gmail.com.pl>
2016-09-05 19:16:44 +0200

    test

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/BartekZapart/BartekZapartStringOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/BartekZapart/BartekZapartStringOperations.cs
@@ -44,5 +44,10 @@ namespace SeeSharpBasics.BartekZapart
         {
             throw new NotImplementedException();
         }
+
+        public void RafalKawalerski(string test)
+        {
+            
+        }
     }
 }
