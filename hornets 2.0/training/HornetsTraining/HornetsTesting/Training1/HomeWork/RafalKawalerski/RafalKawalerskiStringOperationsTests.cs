795022af17094ac5fc2172757aea20db28f4778b
ar-kay <kawalerski.r@gmail.com.pl>
2016-09-08 00:24:38 +0200

    Homework - StringOperations

--- hornets 2.0/training/HornetsTraining/HornetsTesting/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperationsTests.cs
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperationsTests.cs
@@ -20,5 +20,22 @@ namespace HornetsTesting.Training1.HomeWork.RafalKawalerski
             Assert.AreEqual(_rkStringOperations.Substring("AlaMaKota",3,2),"Ma");
             Assert.AreEqual(_rkStringOperations.Substring("House", 3, 5), "Za duza wartosc length");
         }
+
+        [TestMethod]
+        public void PositionInStringTest()
+        {
+            Assert.AreEqual(_rkStringOperations.PositionInString("Hornets","rn"),2);
+            Assert.AreEqual(_rkStringOperations.PositionInString("AlaMaKota","Kota"),5);
+            Assert.AreEqual(_rkStringOperations.PositionInString("Catsanddogs","pig"), -1);
+        }
+
+        [TestMethod]
+        public void StringReplaceTest()
+        {
+            Assert.AreEqual(_rkStringOperations.StringReplace("CatsAndDogs","Dogs","Tigers"),"CatsAndTigers");
+            Assert.AreEqual(_rkStringOperations.StringReplace("Beatka","atka","ta"),"Beta");
+            Assert.AreEqual(_rkStringOperations.StringReplace("NoIdea","Id","Ik"),"NoIkea");
+            Assert.AreEqual(_rkStringOperations.StringReplace("NoIdea","No","Yes"),"YesIdea");
+        }
     }
 }
--- hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperations.cs
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperations.cs
@@ -1,4 +1,7 @@
-﻿namespace HornetsTraining.Training1.HomeWork.RafalKawalerski
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HornetsTraining.Training1.HomeWork.RafalKawalerski
 {
     public class RafalKawalerskiStringOperations : StringOperations
     {
@@ -20,12 +23,41 @@
 
         public override int PositionInString(string candidate, string needle)
         {
-            var position; 
+
+            if (string.IsNullOrEmpty(candidate) || string.IsNullOrEmpty(needle))
+                return -1;
+
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if (candidate[i] == needle[0])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         public override string StringReplace(string candidate, string needle, string replaceWith)
         {
-            throw new System.NotImplementedException();
+            var endOfFirstPart = PositionInString(candidate,needle);
+            var firstPart = Substring(candidate, 0, endOfFirstPart);
+            var textReplace = firstPart + replaceWith;
+            var endOfEnd = endOfFirstPart + needle.Length;
+            var candidateLength = candidate.Length;
+            var text = "";
+
+            if (endOfEnd == candidateLength)
+            {
+                text = textReplace;
+            }
+            else
+            {
+                var endPart = Substring(candidate, endOfEnd, candidateLength - endOfEnd);
+                text = textReplace + endPart;
+            }
+
+            return text;
         }
     }
 }
