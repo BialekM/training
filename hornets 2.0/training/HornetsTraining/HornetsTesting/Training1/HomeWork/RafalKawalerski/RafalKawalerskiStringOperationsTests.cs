2cb5a68651d2858dd67eca549c969a6fa26cc41e
ar-kay <kawalerski.r@gmail.com.pl>
2016-09-07 19:05:31 +0200

    homework

---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperationsTests.cs
@@ -0,0 +1,24 @@
+﻿using HornetsTraining.Training1.HomeWork.RafalKawalerski;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HornetsTesting.Training1.HomeWork.RafalKawalerski
+{
+    [TestClass]
+    public class RafalKawalerskiStringOperationsTests
+    {
+        private readonly RafalKawalerskiStringOperations _rkStringOperations = new RafalKawalerskiStringOperations();
+
+        [TestMethod]
+        public void GetNameTest()
+        {
+            Assert.AreEqual(_rkStringOperations.GetName(), "Rafal Kawalerski");
+        }
+
+        [TestMethod]
+        public void SubstringTest()
+        {
+            Assert.AreEqual(_rkStringOperations.Substring("AlaMaKota",3,2),"Ma");
+            Assert.AreEqual(_rkStringOperations.Substring("House", 3, 5), "Za duza wartosc length");
+        }
+    }
+}
--- hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperations.cs
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/RafalKawalerski/RafalKawalerskiStringOperations.cs
@@ -9,12 +9,18 @@
 
         public override string Substring(string candidate, int start, int length)
         {
-            throw new System.NotImplementedException();
+            var text = "";
+            if (candidate.Length - start < length)
+                return "Za duza wartosc length";
+            for (int i = start; i < start + length; i++)
+                text += candidate[i];
+            
+            return text;
         }
 
         public override int PositionInString(string candidate, string needle)
         {
-            throw new System.NotImplementedException();
+            var position; 
         }
 
         public override string StringReplace(string candidate, string needle, string replaceWith)
