b3e831c02a77cf725ae69e4baa53583e4dd27bbb
Synergia503 <jasiewiczbartosz@gmail.com>
2016-09-08 18:12:50 +0200

    Add files via upload

---
+++ hornets%202.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/Bartosz Jasiewicz/GenericsOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkGenerics
+{
+    public class GenericsOperations<TList> : MyList<int>
+    {
+        protected int[] myListOfInt;
+        int size = 1;
+        public override void Add(int item)
+        {
+            listOfItems = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+
+                myListOfInt[i] = item;
+                size++;
+                return;
+            }
+
+        }
+    }
+}
