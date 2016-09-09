d206254c92487f7149cba6bb4134267e46369529
Wilczy <twilkosz@gmail.com>
2016-09-09 17:00:00 +0200

    .

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/TomaszWilkosz/TomaszWilkoszStringOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornetsTraining.Training1.HomeWork.TomaszWilkosz
+{
+    class TomaszWilkoszStringOperations : StringOperations
+    {
+        public override string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int PositionInString(string candidate, string needle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string StringReplace(string candidate, string needle, string replaceWith)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Substring(string candidate, int start, int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
