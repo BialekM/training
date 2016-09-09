00c959e152668bb7cf1ed8008eb3f281fe1eb9b8
Patryk <patrickmlm41@gmail.com>
2016-09-09 22:14:13 +0200

    zadanie

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/PatrykDrapik/PatrykDrapikStringOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace HornetsTraining.Training1.HomeWork.PatrykDrapik
+{
+    public class PatrykDrapikStringOperations : StringOperations
+    {
+        public override string GetName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string Substring(string candidate, int start, int length)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int PositionInString(string candidate, string needle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string StringReplace(string candidate, string needle, string replaceWith)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
