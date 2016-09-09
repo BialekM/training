43c1a8174fdf4fdac1d4b8cdff55dff7b72a8a5f
Sacharomyces <simson.kenny@gmail.com>
2016-09-10 01:15:16 +0200

    Homework part 1

---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training1/HomeWork/Bartosz Mackieiwcz/BartoszMackiewiczStringOperationsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HornetsTraining.Training1.HomeWork.BartekZapart;
+using HornetsTraining.Training1.HomeWork.BartoszMackiewicz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HornetsTesting.Training1.HomeWork.Bartosz_Mackieiwcz
+{
+    
+    [TestClass]
+    public class BartoszMackiewiczStringOperationsTest
+    {
+        private  BartoszMackiewiczStringOperations Bartosz = new BartoszMackiewiczStringOperations();
+
+        [TestMethod]
+        public void SubstringTest()
+        {
+            Assert.AreEqual(4, Bartosz.PositionInString("GiewTelkiont","Telki"));    
+        }
+
+        [TestMethod]
+        public void subst()
+        {
+            Assert.AreEqual(Bartosz.StringReplace("Warszklawoa", "klawo", "wa"),"Warszawa");
+        }
+
+    }
+
+}
