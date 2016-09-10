fbc9975d1d97f14ee9cf275ad88777dedf56ba94
warrior888 <warriorr@poczta.fm>
2016-09-10 14:57:45 +0200

    zadanie domowe

--- sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/BartekZapart/BartekTablesOperationsTests.cs
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/BartekZapart/BartekTablesOperationsTests.cs
@@ -12,9 +12,12 @@ namespace Toci.SeeSharpBasics.Test.BartekZapart
         {
             var tableOperations = new TableOperations();
 
-            //tableOperations.ReverseTable()
+            var parameter = new[] {1, 2, 3, 4, 5};
+            var result = tableOperations.ReverseTable(parameter); // 5 , 4, 3 ,2 ,1
 
-            //Assert.AreEqual
+            Assert.AreEqual(parameter[0], result[4]);
+            Assert.AreEqual(parameter[1], result[3]);
+            Assert.AreEqual(parameter[3], result[1]);
             //Assert.AreEqual
             //Assert.AreEqual
         }
