616e80112360d87f4faa3e6290faa5266385edb1
Patryk <patrickmlm41@gmail.com>
2016-09-10 12:36:05 +0200

    homework

---
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/PatrykDrapik/PatrykDrapikStringOperations.cs
@@ -0,0 +1,47 @@
+﻿namespace SeeSharpBasics.PatrykDrapik
+{
+    public class PatrykDrapikStringOperations : StringOperations
+    {
+        public override string Substring(string candidate, int start, int length)
+        {
+            string result = "";
+
+            for (int i = start; i < length + start && i < candidate.Length; i++)
+            {
+                result += candidate[i];
+            }
+
+            return result;
+        }
+
+        public override string GetName()
+        {
+            return "Patryk Drapik";
+        }
+
+        public override string GlueBeginingEnd(string candidate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int CountOccurences(string candidate, char needle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int LetterPositionInString(string candidate, char needle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string LetterReplace(string candidate, char needle, char replace)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string StringReplace(string candidate, string needle, string replace)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
