b124e9f3e9a74e23f74b0eb5f74bddc377961b22
krzysiekpe <krzysiek@krzysiekkrzysiek.pl>
2016-09-10 09:55:24 +0200

    commit w trakcie szkolenia

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/KrzysztofPater/KrzysztofPaterMyList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HornetsTraining.Training1.Generics;
+
+namespace HornetsTraining.Training1.HomeWork.KrzysztofPater
+{
+    public class KrzysztofPaterMyList<TListItem> : MyList<TListItem>
+    {
+        protected TListItem[] listOfItems = new TListItem[1];
+        protected int Index = 0;
+
+        public virtual void Add(TListItem item)
+        {
+            listOfItems[Index] = item;
+
+            TListItem[] tempList = new TListItem[Index + 1];
+
+            for (int i = 0; i < listOfItems.Length; i++)
+            {
+                tempList[i] = listOfItems[i];
+            }
+            listOfItems = tempList;
+        }
+
+    }
+}
