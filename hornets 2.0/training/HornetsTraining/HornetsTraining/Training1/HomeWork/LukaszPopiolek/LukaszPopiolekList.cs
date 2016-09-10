757efb9a5115b21febeed721bb5964da5738a101
PopielatyPL <lukasz.pop@outlook.com>
2016-09-11 01:31:26 +0200

    zad1

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/LukaszPopiolek/LukaszPopiolekList.cs
@@ -0,0 +1,26 @@
+﻿using HornetsTraining.Training1.Generics;
+
+namespace HornetsTraining.Training1.HomeWork.PatrykDrapik
+{
+    public class LukaszPopiolekList<TItem> : MyList<TItem>
+    {
+        public LukaszPopiolekList()
+        {
+            listOfItems = new TItem[Index];
+        }
+
+        public override void Add(TItem item)
+        {
+            var lenght = this.listOfItems.Length;
+            TItem[] temp = new TItem[++lenght];
+            for (int i = 0; i < listOfItems.Length; i++)
+            {
+                temp[i] = listOfItems[i];
+            }
+            listOfItems = temp;
+            listOfItems[lenght - 1] = item;
+
+        }
+    }
+}
+}
