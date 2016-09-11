04d18bc951bdb3dd7beb6810c6197e87ebce7cb4
kili-Komputer\kili <g4cester@gmail.com>
2016-09-11 20:00:04 +0200

    no message

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomekStringOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomekStringOperations.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SeeSharpBasics.TomaszKilian
 {
-    public class TomekStringOperations : StringOperations;
+    public class TomekStringOperations : StringOperations
     {
     }
 }
