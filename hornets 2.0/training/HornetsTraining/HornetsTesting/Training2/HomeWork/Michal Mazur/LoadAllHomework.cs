3d490c01cc5c1d58c87a7ca81d8b14feaded355c
Mazur <michalmazur3@gmail.com>
2016-09-14 15:19:38 +0200

    test StringOperations

---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training2/HomeWork/Michal Mazur/LoadAllHomework.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using HornetsTesting.Common;
+
+using HornetsTraining.Training1.HomeWork;
+
+namespace HornetsTesting.Training2.Delegates.MichalMazur
+{
+    public class LoadAllHomework
+    {
+        public delegate string SubstringDelegate(string candidate, int start, int lenght);
+        public delegate int PositionInStringDelegate(string candidate, string needle);
+        public delegate string StringReplaceDelegate(string candidate, string needle, string replaceWith);
+
+        public static List<StringOperations> LoadFromAssembly()
+        {
+            AssemblyLoader al = new AssemblyLoader();
+
+            var instances = al.GetTypesInstancesList<StringOperations>();
+
+            return (List<StringOperations>)instances;
+        }
+
+
+        public static SubstringDelegate AppendSubstringMethod(List<StringOperations> list)
+        {
+             SubstringDelegate del=null;
+            foreach (var item in list)
+            {
+                del += item.Substring;
+            }
+
+            return del;
+        }
+
+        //public Dictionary<string, string> TestAll(List<StringOperations> list,Func<string, int, int, string> testDelegate)
+        //{
+        //    Dictionary<string, string> dict = new Dictionary<string, string>();
+        //    foreach (var item in list)
+        //    {
+                
+        //    }
+            
+       // }
+
+    }
+}
---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training2/HomeWork/Michal Mazur/MichalMazurStringOperationsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using HornetsTesting.Common;
+using HornetsTraining.Training1.HomeWork;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace HornetsTesting.Training2.Delegates.MichalMazur
+{
+    [TestClass]
+    public class MichalMazurStringOperationsTest
+    {
+        [TestMethod]
+        public void  TestHomework()
+        {
+            List<StringOperations> homework = LoadAllHomework.LoadFromAssembly();
+
+            LoadAllHomework.SubstringDelegate delSubstring = LoadAllHomework.AppendSubstringMethod(homework);
+            delSubstring("komputer", 2, 3);
+
+
+
+
+
+        }
+    }
+}
