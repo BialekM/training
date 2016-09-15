e8e6c3dff39da7442f019c324360180e08b543cb
kili-Komputer\kili <g4cester@gmail.com>
2016-09-16 01:17:56 +0200

    no message

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomaszKilTablesOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomaszKilTablesOperations.cs
@@ -7,9 +7,9 @@ namespace SeeSharpBasics.TomaszKilian
         public override int[] BubbleSort(int[] tableToSort)
         {
             int counter = '0';
-            bool tellmeend = false;
-            for (int i = 0; i < tableToSort.Length; i++)
-            {
+           
+            
+            
                 for (int j = 0; j < tableToSort.Length; j++)
                 {
                     if (tableToSort[j] > tableToSort[j + 1])
@@ -17,13 +17,12 @@ namespace SeeSharpBasics.TomaszKilian
                         tableToSort[j] = counter;
                         tableToSort[j] = tableToSort[j + 1];
                         tableToSort[j + 1] = counter;
-                        tellmeend = false;
+                        
                     }
                 }
-                if (tellmeend == true)
-                    break;
+               
 
-            }
+            
             return tableToSort;
 
         }
