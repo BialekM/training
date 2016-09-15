270020f581943a78ff10a6b221def1143fa92f34
Mazur <michalmazur3@gmail.com>
2016-09-15 19:57:42 +0200

    homework BankTransfer

--- hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/Michal Mazur/Ing/IngTransferResolver.cs
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/Michal Mazur/Ing/IngTransferResolver.cs
@@ -1,10 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using HornetsTraining.Training2.HomeWork.Ing;
 
-namespace HornetsTraining.Training2.HomeWork.Michal
+namespace HornetsTraining.Training2.HomeWork.MichalMazur
 {
     public class IngTransferResolver : TransferResolver
     {
-         
+        public override void DoTransfer(Transfer transfer)
+        {
+            string DestinationBankID = transfer.DestinationBankAccount.Substring(2, 4);
+            string SourceBankID = transfer.SourceBankAccount.Substring(2, 4);
+            bool StatusTransfer = false;
+            
+            var factory = new IngTransferHandlerFactory();
+            TransferHandler handler = factory.GetInstance(DestinationBankID);
+
+           if((StatusTransfer = ChechValidBankID(handler, transfer, DestinationBankID, SourceBankID))==true)
+           {
+                Debug.WriteLine("sent money" );
+           }
+           else
+           {
+                 Debug.WriteLine("error");
+           }
+      
+        }
+
+        private bool ChechValidBankID(TransferHandler handler, Transfer transfer, string Destination, string Source)
+        {
+            if (handler != null)
+            {
+                ChooseTransferType(handler, transfer, Destination, Source);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ChooseTransferType(TransferHandler handler, Transfer t, string Destination, string Source)
+        {
+
+            if (Destination==Source)
+            {
+   
+                //handler.DoInTransfer(t);
+      
+                
+            }
+            else
+            {
+               // handler.DoOutTransfer(t);
+            }
+        }
     }
 }
 
