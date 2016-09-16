943156b66f59866958c23e4e2b698929b112a587
kili-Komputer\kili <g4cester@gmail.com>
2016-09-16 21:57:57 +0200

    kosmetyka

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomaszKilianStringOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomaszKilianStringOperations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -11,96 +12,106 @@ namespace SeeSharpBasics.TomaszKilian
     {
         public override string Substring(string candidate, int start, int length)
         {
-            string wynikowa = "";
-            for (int i = start; i <= length ; i++)
+            string result = "";
+            for (int i = start; i <= length; i++)
             {
-                wynikowa += candidate[i];
+                result += candidate[i];
             }
-            return wynikowa;
+            return result;
 
 
 
         }
 
-    public override string GetName()
-       {
-           throw new NotImplementedException();
-       }
+        public override string GetName()
+        {
+            throw new NotImplementedException();
+        }
 
-       public override string GlueBeginingEnd(string candidate)
-       {
-           string wynikowa = "";
-           for (int i = 0, j = candidate.Length - 1; i < j; i++ ,j--)
-           {
-               wynikowa += candidate[i];
-               wynikowa += candidate[j];
-
-
-
-           }
-           return wynikowa;
-       }
-
-       public override int CountOccurences(string candidate, char needle)
-       {
-           int ileliter = 0;
-           int start = 0;
-           for (int i = 0; i < candidate.Length; i++)
-           {
-               if (candidate[i] == needle)
-               {
-                   start++;
-                   
-               }
-            
-              
-           }
-           return start;
+        public override string GlueBeginingEnd(string candidate)
+        {
+            string result = "";
+            for (int i = 0, j = candidate.Length - 1; i < j; i++ ,j--)
+            {
+                result += candidate[i];
+                result += candidate[j];
 
 
-       }
 
-       public override int LetterPositionInString(string candidate, char needle)
-       {
-           int start = 0;
-           for (int i = 0; i < candidate.Length; i++)
-           {
-               if (candidate[i] == needle)
-               {
-                   start = i;
-               }
+            }
+            return result;
+        }
+
+        public override int CountOccurences(string candidate, char needle)
+        {
            
-           }
-           return start;
+            int start = 0;
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if (candidate[i] == needle)
+                {
+                    start++;
+
+                }
+
+
+            }
+            return start;
+
+
+        }
+
+        public override int LetterPositionInString(string candidate, char needle)
+        {
+            int start = 0;
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if (candidate[i] == needle)
+                {
+                    start = i;
+                }
+
+            }
+            return start;
 
 
 
-       }
+        }
 
-       public override string LetterReplace(string candidate, char needle, char replace)
-       {
-           string pomocniczy = "";
-           
-
-           for (int i = 0; i < candidate.Length; i++)
-           {
-               if (candidate[i] != needle)
-               {
-                   pomocniczy += candidate[i];
-               }
-               else 
-               {
-                   pomocniczy = pomocniczy + replace;
-               }
-           }
-           return pomocniczy;
+        public override string LetterReplace(string candidate, char needle, char replace)
+        {
+            string pomocniczy = "";
 
 
-       }
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if (candidate[i] != needle)
+                {
+                    pomocniczy += candidate[i];
+                }
+                else
+                {
+                    pomocniczy = pomocniczy + replace;
+                }
+            }
+            return pomocniczy;
 
-       public override string StringReplace(string candidate, string needle, string replace)
-       {
-           throw new NotImplementedException();
-       }
+
+        }
+
+
+        public override string StringReplace(string candidate, string needle, string replace)
+            //bartlomiej, art, beatka => bbeatkalomiej 
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
+
+
+
 }
+    
+
+
---
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/TomaszKil/TomaszKilianStringTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeSharpBasics.TomaszKilian;
+
+namespace Toci.SeeSharpBasics.Test.TomaszKil
+{[TestClass]
+    public class TomaszKilianStringTests:TomaszKilianStringOperations
+    {
+         
+    }
+}
+
+
+
+
+
