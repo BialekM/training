cbd9443ddf312842e80512d9b8d64d6fba01b3f0
qajus <magdalena.skalik@gmail.com>
2016-09-16 15:05:22 +0200

    poprawa zadania domowego

--- sparrows/Training1/SeeSharpBasics/SeeSharpBasics/MagdaSkalik/MagdaSkalikStringOperations.cs
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/MagdaSkalik/MagdaSkalikStringOperations.cs
@@ -4,24 +4,7 @@ namespace SeeSharpBasics.MagdaSkalik
 {
     public class MagdaSkalikStringOperations : StringOperations
     {
-        public override string Substring(string candidate, int start, int length); // beatka, 1, 3 => eat ; bartek, 2, 4 => rtek
-
-        public override string GetName()
-        {
-            return "Magda Skalik";
-        }// zwrocic swoje imie i nazwisko
-
-        public override string GlueBeginingEnd(string candidate); // bartlomiej  bjaeritmlo
-
-        public override int CountOccurences(string candidate, char needle); // bartek, a => 1 beatka, a => 2, aaaaba, a => 5
-
-        public override int LetterPositionInString(string candidate, char needle); // bartek, k => 5
-
-        public override string LetterReplace(string candidate, char needle, char replace); // beatka, e, s => bsatka ; beatka , a , b => bebtkb
-
-        public override string StringReplace(string candidate, string needle, string replace); // bartlomiej, art, beatka => bbeatkalomiej
-        
-                public static string Substring(string candidate, int start, int length)
+        public override string Substring(string candidate, int start, int length)
         {
             string fragment = "";
             for(int i = start; i< (start+length) && i< candidate.Length;i++)
@@ -30,7 +13,52 @@ namespace SeeSharpBasics.MagdaSkalik
                 fragment+=candidate[i];
             }
             return fragment;
+        }// beatka, 1, 3 => eat ; bartek, 2, 4 => rtek
+
+        public override string GetName()
+        {
+            return "Magda Skalik";
+        }// zwrocic swoje imie i nazwisko
+
+        public override string GlueBeginingEnd(string candidate)
+        {
+            string result = "";
+            int candLen = candidate.Length;
+            for (int i = 0; i < candLen / 2; i++)
+            {
+                result = result + candidate[i] + candidate[candLen - 1 - i];
+            }
+                if (candLen % 2 == 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    result = result + candidate[(candidate.Length / 2)];
+                    return result;
+                }
+
         }
+
+        public override int CountOccurences(string candidate, char needle); // bartek, a => 1 beatka, a => 2, aaaaba, a => 5
+
+        public override int LetterPositionInString(string candidate, char needle)
+        {
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if(candidate[i]==needle)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }// bartek, k => 5
+
+        public override string LetterReplace(string candidate, char needle, char replace); // beatka, e, s => bsatka ; beatka , a , b => bebtkb
+
+        public override string StringReplace(string candidate, string needle, string replace); // bartlomiej, art, beatka => bbeatkalomiej
+        
+       
         public static string ReplaceInText(string candidate, string find, string replace)
         {
             string text = "";
