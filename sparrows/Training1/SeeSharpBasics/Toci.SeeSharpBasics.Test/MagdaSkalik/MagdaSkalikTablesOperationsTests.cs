55418bae8bb22777de3c824a21e81a18daeb5552
qajusLap <magdalena.skalik@gmail.com>
2016-09-16 22:24:22 +0200

    dodanie unitTests

---
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/MagdaSkalik/MagdaSkalikTablesOperationsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.SeeSharpBasics.Test.MagdaSkalik
+{
+    [TestClass]
+    public class MagdaSkalikTablesOperationsTests : TablesOperationsTests
+    {
+        [TestMethod]
+        public void BubbleSort()
+        {
+
+        }
+    }
+}
