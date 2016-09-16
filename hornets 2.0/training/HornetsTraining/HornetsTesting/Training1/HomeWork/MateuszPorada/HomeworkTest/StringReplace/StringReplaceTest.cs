4b574af5d2f1979fe1e1e1a80a8f93c56073a9f5
Mateusz Porada <mateuszporada94@outlook.com>
2016-09-16 13:24:46 +0200

    Homework testing

---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training1/HomeWork/MateuszPorada/HomeworkTest/StringReplace/StringReplaceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace HornetsTesting.Training1.HomeWork.MateuszPorada.HomeworkTest.StringReplace
+{
+    public class StringReplaceTest
+    {
+        public string Candidate { get; set; }
+
+        public string Needle { get; set; }
+
+        public string ReplaceWith { get; set; }
+
+        public int Start { get; set; }
+
+        public int Length { get; set; }
+
+        public string Expected { get; set; }
+    }
+}
