f0f257e0a056214b510cf156500d84b64189a3c4
ŁukaszCichoń <lukasz.cichon11@gmail.com>
2016-09-16 22:57:13 +0200

    HomeWork

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/LukaszCichon/AliorBank/AliorBankTransferHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace HornetsTraining.Training2.HomeWork.LukaszCichon.AliorBank
+{
+    public class AliorBankTransferHandler : TransferHandler
+    {
+        public override bool DoInTransfer(Transfer transfer)
+        {
+            StreamWriter sw = new StreamWriter(@"..\..\Training2\HomeWork\LukaszCichon\HistoriaOperacji.txt", true);
+
+            sw.WriteLine("-----------------------------------------------------------------");
+            sw.WriteLine("Transakcja wewnetrzna");
+            sw.WriteLine("Data: {0}", DateTime.Now);
+            sw.WriteLine("Numer konta docelowego: {0}", transfer.DestinationBankAccount);
+            sw.WriteLine("Numer konta zrodlowego: {0}", transfer.SourceBankAccount);
+            sw.WriteLine("Pieniadze: {0}", transfer.Money);
+            sw.WriteLine();
+            sw.Close();
+            
+            return (true);
+        }
+
+        public override bool DoOutTransfer(Transfer transfer)
+        {
+            StreamWriter sw = new StreamWriter(@"..\..\Training2\HomeWork\LukaszCichon\HistoriaOperacji.txt", true);
+
+            sw.WriteLine("-----------------------------------------------------------------");
+            sw.WriteLine("Transakcja zewnetrzna");
+            sw.WriteLine("Data: {0}", DateTime.Now);
+            sw.WriteLine("Numer konta docelowego: {0}", transfer.DestinationBankAccount);
+            sw.WriteLine("Numer konta zrodlowego: {0}", transfer.SourceBankAccount);
+            sw.WriteLine("Pieniadze: {0}", transfer.Money);
+            sw.WriteLine();
+            sw.Close();
+
+            return (true);
+        }
+    } 
+}
---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/LukaszCichon/AliorBank/AliorBankTransferHandlerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using HornetsTraining.Training1.Generics.Factory;
+using HornetsTraining.Training2.HomeWork.BankMillenium;
+using HornetsTraining.Training2.HomeWork.LukaszSmierzchala.ToyotaBank;
+using HornetsTraining.Training2.HomeWork.Mbank;
+using HornetsTraining.Training2.HomeWork.MichalMazur;
+using HornetsTraining.Training2.HomeWork.NBP;
+
+namespace HornetsTraining.Training2.HomeWork.LukaszCichon.AliorBank
+{
+    public class AliorBankTransferHandlerFactory  : DelegateFactoryBase<TransferHandler>
+    {
+        public AliorBankTransferHandlerFactory()
+        {
+            ListOfInstances = new Dictionary<string, Func<TransferHandler>>()
+            {
+                {"2490", () => new AliorBankTransferHandler()},
+                {"2160", () => new ToyotaBankTransferHandler()},
+                {"1440", () => new NordeaBankTransferHandler()},
+                {"1140", () => new MBankTansferHandler()},
+                {"1680", () => new PlusBankTransferHandler()},
+                {"1050", () => new IngTransferHandler()},
+                {"1010", () => new NBPTransferHandler()},
+                {"1160", () => new BankMilleniumTransferHandler()}
+            };
+        }
+    }
+}
