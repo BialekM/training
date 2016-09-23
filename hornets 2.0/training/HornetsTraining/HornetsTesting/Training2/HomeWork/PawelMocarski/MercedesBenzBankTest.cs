c78b6be468b6597d293f13d8fdb388d2ac2d3012
Pawel Mocarski <mocarski.pawel@gmail.com>
2016-09-23 22:22:17 +0200

    homework

---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training2/HomeWork/PawelMocarski/MercedesBenzBankTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HornetsTraining.Training2.HomeWork;
+using HornetsTraining.Training2.HomeWork.PawelMocarski.MercedesBenzBank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HornetsTesting.Training2.HomeWork.PawelMocarski
+{
+    [TestClass]
+    public class MercedesBenzBankTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var transfer = new Transfer { DestinationBankAccount = "101090436763276523475234", SourceBankAccount = "10158050437285943", Money = 1800 };
+
+            TransferResolver transferResolver = new MercedesBenzBankTransferResolver();
+
+            transferResolver.DoTransfer(transfer);
+        }
+    }
+}
---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/PawelMocarski/MercedesBenzBank/MercedesBenzBankFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HornetsTraining.Training1.Generics.Factory;
+using HornetsTraining.Training2.HomeWork.BankMillenium;
+using HornetsTraining.Training2.HomeWork.BZWBK;
+using HornetsTraining.Training2.HomeWork.LukaszCichon.AliorBank;
+using HornetsTraining.Training2.HomeWork.LukaszSmierzchala.ToyotaBank;
+using HornetsTraining.Training2.HomeWork.MarcinJaniak;
+using HornetsTraining.Training2.HomeWork.Mbank;
+using HornetsTraining.Training2.HomeWork.MichalMazur;
+using HornetsTraining.Training2.HomeWork.NBP;
+using HornetsTraining.Training2.HomeWork.PKOBP;
+using HornetsTraining.Training2.HomeWork.SantanderConsumerBank;
+
+namespace HornetsTraining.Training2.HomeWork.PawelMocarski.MercedesBenzBank
+{
+    public class MercedesBenzBankFactory : DelegateFactoryBase<TransferHandler>
+    {
+        public MercedesBenzBankFactory()
+        {
+            ListOfInstances = new Dictionary<string, Func<TransferHandler>>()
+            {
+                {"1160", () => new BankMilleniumTransferHandler()},
+                {"1060", () => new BPHTransferHandler() },
+                {"1090", () => new BZWBKTransferHandler() },
+                {"2490", () => new AliorBankTransferHandler() },
+                {"2160", () => new ToyotaBankTransferHandler() },
+                {"2000", () => new RabobankPolskaTransferHandler() },
+                {"1140", () => new MBankTransferHandler() },
+                {"1050", () => new IngTransferHandler() },
+                {"1010", () => new NBPTransferHandler() },
+                {"1440", () => new NordeaBankTransferHandler() },
+                {"1580", () =>  new MercedesBenzBankTransferHandler() },
+                {"1240", () => new PkobpTransferHandler() },
+                {"1680", () => new PlusBankTransferHandler() },
+                {"2120", () => new SantanderConsumerBankTransferHandler() }
+            };
+
+        }
+    }
+}
