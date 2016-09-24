29be839f06aafbd8db0d75e262f6fdcd5761e6ce
qajusLap <magdalena.skalik@gmail.com>
2016-09-24 12:46:19 +0200

    poprawka zadania

--- sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/MagdaSkalik/MagdaSkalikTablesOperationsTests.cs
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/MagdaSkalik/MagdaSkalikTablesOperationsTests.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeSharpBasics.MagdaSkalik;
+using Toci.SeeSharpBasics.Test.TablesOperations;
 
 namespace Toci.SeeSharpBasics.Test.MagdaSkalik
 {
@@ -9,6 +10,11 @@ namespace Toci.SeeSharpBasics.Test.MagdaSkalik
         [TestMethod]
         public void BubbleSort()
         {
+            MagdaSkalikTableOperations test = new MagdaSkalikTableOperations();
+
+            var parameter = new[] { 3, 6, 2, 4, 1 };
+            var result = test.BubbleSort(parameter);
+            Assert.AreEqual(parameter[0], result[1]);
 
         }
     }
