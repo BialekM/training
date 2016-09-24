b2111fb767137305946a257ef34a129bd161f511
qajusLap <magdalena.skalik@gmail.com>
2016-09-24 13:15:37 +0200

    poprawka

--- sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/MagdaSkalik/MagdaSkalikTablesOperationsTests.cs
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/MagdaSkalik/MagdaSkalikTablesOperationsTests.cs
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SeeSharpBasics.MagdaSkalik;
-=======
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
->>>>>>> 4b127e114a1a7e781fdbadda04db1a44345ce4a4
 using Toci.SeeSharpBasics.Test.TablesOperations;
+using SeeSharpBasics.MagdaSkalik;
 
 namespace Toci.SeeSharpBasics.Test.MagdaSkalik
 {
