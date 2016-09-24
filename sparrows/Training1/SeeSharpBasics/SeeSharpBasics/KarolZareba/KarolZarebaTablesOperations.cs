b609ed9ad4325dc1898f736980ce3349e31dad27
karolzareba <karol.zareba01@gmail.com>
2016-09-24 12:50:12 +0200

    commit

---
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/KarolZareba/KarolZarebaTablesOperations.cs
@@ -0,0 +1,16 @@
+﻿namespace SeeSharpBasics.KarolZareba
+{
+    public class KarolZarebaTablesOperations
+    {
+        public int[] GetElemenInts(int[] table, int greaterThan) //table = 1,2,3,4,5,6,7,8,9 gratherThan = 5-> 6,7,8,9
+        {
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] > greaterThan)
+                {
+                    //dodajemy do wynikowej tablicy
+                }
+            }
+        }
+    }
+}
---
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/KarolZareba/KarolZarebaTablesOperationsTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.SeeSharpBasics.Test.TablesOperations;
+
+namespace Toci.SeeSharpBasics.Test.KarolZareba
+{
+
+    [TestClass]
+    public class KarolZarebaTablesOperationsTests : TablesOperationsTests
+    {
+        [TestMethod]
+        public override void TestReversing()
+        {
+            var tableOperations = new TablesOperations();
+        }
+    }
+}
