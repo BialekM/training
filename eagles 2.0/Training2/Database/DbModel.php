19722f866675570e897561aa72fc21b02f25ca39
Marcin-Komputer\Marcin <marcin.galinski1705@gmail.com>
2016-09-25 08:08:27 +0200

    Select

--- eagles 2.0/Training2/Database/DbModel.php
+++ eagles 2.0/Training2/Database/DbModel.php
@@ -6,6 +6,16 @@
 		
 		protected $fields = array();
 		
+		protected $isLike = "";
+		
+		protected $isLimit = "";
+		
+		protected $isGroup = "";
+		
+		protected $isOrder = "";
+		
+		protected $operator;
+		
 		public function GetFields()
 		{
 			return $this->fields;
@@ -29,5 +39,61 @@
 		public function GetFild($name)
 		{
 			return $this->fields[$name];
+		}
+		
+		
+		/////////////////SELECT///////////////////////////
+		public function IsLike($pattern)
+		{
+			$this->isLike = "like '" . $pattern . "'";
+			return $this;
+		}
+		public function GetLike()
+		{
+			return $this->isLike;
+		}
+		public function IsLimit($limits = array())
+		{
+			$this->isLimit = "limit ";
+			foreach ($limits as $limitsKey => $limitsValue )
+			{
+				$this->isLimit .= $limitsValue;
+				$this->isLimit .= ",";
+			}
+			$this->isLimit = rtrim($this->isLimit, ',');
+		}
+		public function GetLimit()
+		{
+			return $this->isLimit;
+		}
+		public function IsGroup($category)
+		{
+			$this->isGroup = "group by " . $category;
+		}
+		public function GetGroup()
+		{
+			return $this->isGroup;
+		}
+		public function IsOrder($args = array())
+		{
+			$this->isOrder = "order by ";
+			foreach($args as $argsKey => $argsValue)
+			{
+				$this->isOrder .= $argsValue;
+				$this->isOrder .= ",";
+			}
+			$this->isOrder = rtrim($this->isOrder,',');
+		}
+		public function GetOrder()
+		{
+			return $this->isOrder;
+		}
+		public function SetOperator($operator)
+		{
+			$this->operator = $operator;
+		}
+		public function GetOperator()
+		{
+			return $this->operator;
 		}
 	}
--- eagles 2.0/Training2/Database/PostgreSqlSelect.php
+++ eagles 2.0/Training2/Database/PostgreSqlSelect.php
@@ -2,5 +2,21 @@
 
 	class PostgreSqlSelect extends DbWhere
 	{
+		protected $selectCommand = "select ";
+		protected $fromCommand = " from ";
 		
+		public function Select(IDbModel $model)
+		{
+			$resLike = $model->GetLike();
+			$resLimit = $model->GetLimit();
+			$resGroup = $model->GetGroup();
+			$resOrder = $model->GetOrder();
+				
+			$select = $this->selectCommand . $model->GetOperator() . $this->fromCommand . $model->GetTableName() . " "
+					//. $this->GetWhereClause($model);
+			. ($this->GetWhereClause($model) ? $this->GetWhereClause($model) : '')
+			. ( isset($resLike) ? $resLike : '') . " " . ( isset($resLimit) ? $resLimit : '')
+			. " " . ( isset($resGroup) ? $resGroup : '') . " " . ( isset($resOrder) ? $resOrder : ''). " ";
+			return $select;
+		}
 	}
