e0ab72f157e762285fedb04bf8239e2122c39f4f
Mazur <michalmazur3@gmail.com>
2016-09-26 13:08:39 +0200

    .

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training3/Homework/MichalMazur/Reflection/MichalMazurReflectionHomework.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace HornetsTraining.Training3.Homework.MichalMazur.Reflection
+{
+    public class MichalMazurReflectionHomework : TrainingThreeHomework
+    {
+        
+
+        public override void RunGenericMethods(Dictionary<string, string> keyMethodNameValueTypeName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateObject()
+        {
+            Type type = Type.GetType("HornetsTraining.Training3.Homework.GenericClass, Toci.HornetsTraining");
+            var myGenericClass = Activator.CreateInstance(type);
+
+            MethodInfo[] info = myGenericClass.GetType().GetMethods(BindingFlags.Instance);
+        }
+    }
+}
