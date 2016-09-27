9f659a3aae1ca9d0665557f9a37c4d0851f34b57
Adrian Pankiewicz <farami11@tlen.pl>
2016-09-27 21:59:27 +0200

    Homework - 1000 TPL transfers test PASSED :)

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training3/Homework/AdrianPankiewicz/AdrianPankiewiczTplHomeWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using HornetsTraining.Training2.HomeWork;
+using HornetsTraining.Training2.HomeWork.Mbank;
+using HornetsTraining.Training3.Homework.Tpl;
+
+namespace HornetsTraining.Training3.Homework.AdrianPankiewicz
+{
+    public class AdrianPankiewiczTplHomeWork : TplHomework
+    {
+        
+        public override void DoTransfers()
+        {
+            TaskFactory taskFactory = new TaskFactory();
+            TransferResolver transferResolver = new MBankTranferResolver();
+            for (int i = 0; i < 1000; i++)
+            {
+                Transfer newTransfer = RandomTransferGenerator.GenerateRandomTransfer();
+                taskFactory.StartNew(() => transferResolver.DoTransfer(newTransfer));
+            }
+
+        }
+
+    }
+}
