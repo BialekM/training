4fc2272da3baf7d8942d3d616ca88288909a0d79
ŁukaszCichoń <lukasz.cichon11@gmail.com>
2016-09-28 16:56:32 +0200

    HomeworkUpdate

--- hornets 2.0/training/HornetsTraining/HornetsTesting/Training3/Homework/LukaszCichon/LukaszCichonHomeworkReflectionTest.cs
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training3/Homework/LukaszCichon/LukaszCichonHomeworkReflectionTest.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace HornetsTesting.Training3.Homework.LukaszCichon
 {
     [TestClass]
-    public class LukaszCichonHomeworkTest
+    public class LukaszCichonHomeworkReflectionTest
     {
         private Dictionary<string, string> keyMethodNameValueTypeName = new Dictionary<string, string>()
         {
@@ -24,7 +24,7 @@ namespace HornetsTesting.Training3.Homework.LukaszCichon
         [TestMethod]
         public void RunGenericMethod()
         {
-            LukaszCichonHomework testRun = new LukaszCichonHomework();
+            LukaszCichonReflectionHomework testRun = new LukaszCichonReflectionHomework();
 
             testRun.RunGenericMethods(keyMethodNameValueTypeName);
         }
---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/Training3/Homework/LukaszCichon/LukaszCichonHomeworkTplTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.HornetsTraining.Training3.Homework.LukaszCichon;
+
+namespace HornetsTesting.Training3.Homework.LukaszCichon
+{
+    [TestClass]
+    public class LukaszCichonHomeworkTplTest
+    {
+        [TestMethod]
+        public void testTpl()
+        {
+            LukaszCichonTplHomework tpl = new LukaszCichonTplHomework();
+
+            tpl.DoTransfers();
+        }
+         
+    }
+}
