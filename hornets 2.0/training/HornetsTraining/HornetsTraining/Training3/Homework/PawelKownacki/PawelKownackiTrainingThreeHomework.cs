6e5a4a16d5a4fb48b51518218f09f1c3f1c0e6d8
Pawel_Kownacki <pkownacki90@gmail.com>
2016-09-30 10:12:45 +0200

    ReflectionHomework

--- hornets 2.0/training/HornetsTraining/HornetsTraining/Training3/Homework/PawelKownacki/PawelKownackiTrainingThreeHomework.cs
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training3/Homework/PawelKownacki/PawelKownackiTrainingThreeHomework.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Toci.HornetsTraining.Training3.Homework.PawelKownacki
 {
@@ -6,10 +9,20 @@ namespace Toci.HornetsTraining.Training3.Homework.PawelKownacki
     {
         public override void RunGenericMethods(Dictionary<string, string> keyMethodNameValueTypeName)
         {
-            throw new System.NotImplementedException();
+            keyMethodNameValueTypeName.AsParallel().WithDegreeOfParallelism(4).ForAll(
+                (pair) => RunGenericMethod<GenericClasses>(pair.Key, pair.Value));
+           
+
+
         }
 
-       
-       
+        private void RunGenericMethod<TSource>(string methodName, string methodType)
+        {
+            MethodInfo genMethod = typeof(TSource).GetMethod(methodName).MakeGenericMethod(Type.GetType(methodType));
+            TSource objectExecutedOn = Activator.CreateInstance<TSource>();
+            genMethod.Invoke(objectExecutedOn, null);
+            
+            
+        }
     }
 }
