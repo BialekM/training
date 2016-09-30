452831acffa095e9e3607058a4c6934cd43c308a
SAMSUNG550P5C\Andrew <anw93iw@gmail.com>
2016-09-30 15:47:45 +0200

    no message

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training1/HomeWork/AndrzejIwanski/AndrzejIwanskiStringOperations.cs
@@ -0,0 +1,63 @@
+﻿namespace Toci.HornetsTraining.Training1.HomeWork.AndrzejIwanski
+{
+    public class AndrzejIwanskiStringOperations : StringOperations
+    {
+        public override string GetName()
+        {
+            return "Andrzej Iwanski";
+        }
+
+        public override string Substring(string candidate, int start, int length)
+        {
+            string result = "";
+
+            for (int i = start; i < start + length; ++i)
+            {
+                result += candidate[i];
+            }
+            return result;
+        }
+
+        public override int PositionInString(string candidate, string needle)
+        {
+            int position;
+
+            for (int i = 0; i < candidate.Length; ++i)
+            {
+                if (Substring(candidate, i, needle.Length) == needle)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public override string StringReplace(string candidate, string needle, string replaceWith)
+        {
+            string result = "";
+            int positionStart = PositionInString(candidate, needle);
+            if (positionStart == -1)
+            {
+                return "Brak napisu do podmiany!";
+            }
+
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if (i == positionStart || (i == positionStart && replaceWith.Length == 0))
+                {
+                    result += replaceWith;
+                    i += needle.Length-1;
+                }
+                else
+                {
+                    result += candidate[i];
+                }
+            }
+            return result;
+        }
+
+
+
+        
+    }
+}
