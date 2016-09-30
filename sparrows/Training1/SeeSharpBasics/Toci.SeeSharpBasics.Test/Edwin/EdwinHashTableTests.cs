f3ee3fd6d5000b51848279e6780ac06ad65506eb
Edwin <wieszcz_pl@yahoo.com>
2016-09-30 07:30:21 +0200

    homework-part1

---
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/Edwin/EdwinHashTableTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeSharpBasics.Edwin.HomeWorkHashTables;
+
+namespace Toci.SeeSharpBasics.Test.Edwin
+{
+    [TestClass]
+    public class EdwinHashTableTests
+    {
+        [TestMethod]
+        public void TestM()
+        {
+            EdwinBankResolver test = new EdwinBankResolver();
+            string result = test.GetBankCodeForAccount("124124124124");
+
+            string result2 = test.GetBankName("1140");
+        }
+    }
+}
