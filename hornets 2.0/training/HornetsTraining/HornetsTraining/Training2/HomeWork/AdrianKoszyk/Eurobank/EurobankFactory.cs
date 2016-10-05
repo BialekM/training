07fd0b462f47ac19470e03d1e066b2066e13e896
Adrian Koszyk <adriankoszyk94@gmail.com>
2016-10-05 21:24:36 +0200

    homework2

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/AdrianKoszyk/Eurobank/EurobankFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Toci.HornetsTraining.Training1.Generics.Factory;
+
+namespace Toci.HornetsTraining.Training2.HomeWork.AdrianKoszyk.Eurobank
+{
+    public class EurobankFactory : DelegateFactoryBase<TransferHandler>
+    {
+
+        public EurobankFactory()
+        {
+
+            ListOfInstances = BankList.GetBankList();
+        }
+    }
+}
---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/AdrianKoszyk/Eurobank/EurobankTransfersResolver.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Toci.HornetsTraining.Training2.HomeWork.AdrianKoszyk.Eurobank
+{
+    public class EurobankTransfersResolver : TransferResolver
+    {
+        public override void DoTransfer(Transfer transfer)
+        {
+
+            var handler = GetBank(transfer);
+
+            handler.DoInTransfer(transfer);
+            handler.DoOutTransfer(transfer);
+            
+        }
+
+
+        private TransferHandler GetBank(Transfer transfer)
+        {
+            var list = BankList.GetBankList();
+            var key = GetBankId(transfer.SourceBankAccount);
+            return list[key]();
+        }
+
+      
+
+        public string GetBankId(string number)
+        {
+
+            return number.Substring(2, 4);
+        }
+
+    }
+}
