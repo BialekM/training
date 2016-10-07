a7200059953a16fdfe56e2973a4b9f45d5c8f4c5
Adrian Pankiewicz <farami11@tlen.pl>
2016-10-07 20:24:57 +0200

    TeamLeasing hornets homework

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training4/HomeWork/AdrianPankiewicz/AdrianPankiewiczTeamLeasingInfoTranslation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Xml;
+using Toci.HornetsTraining.Training4.DependencyInjection;
+
+namespace Toci.HornetsTraining.Training4.HomeWork
+{
+    public class AdrianPankiewiczTeamLeasingInfoTranslation : TeamLeasingInfoTranslation
+    {
+
+        public AdrianPankiewiczTeamLeasingInfoTranslation(string language) : base(language)
+        {
+             
+        }
+
+        public override Dictionary<string, string> GetTranslations(List<string> keys)
+        {
+            Dictionary<string, string> translations = ReadDictionary();
+            Dictionary<string, string> listOrdererTranslations = new Dictionary<string, string>();
+
+            keys.ForEach( (key) => listOrdererTranslations.Add(key, translations[key]));
+            return listOrdererTranslations;
+        }
+
+        protected override Dictionary<string, string> ReadDictionary()
+        {
+            return TeamLeasingFileReader.ReadDictionary(String.Format(FileNamePattern, Language));
+        }
+
+    }
+}
