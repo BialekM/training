4882caef20f1de3cbabc2915c3ac572204c52602
Sachio0 <nowi.king@gmail.com>
2016-10-08 12:58:41 +0200

    fix Homework

---
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/SlawekNowinski/SlawekNowinskiPostgresqlDbClient.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using SeeSharpBasics.Training4.Db;
+
+namespace SeeSharpBasics.SlawekNowinski
+{
+    public class SlawekNowinskiPostgresqlDbClient : PostgresqlDbClient
+    {
+        public override bool Update(string query)
+        {
+            Connection.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand(query);
+            command.Connection = Connection;
+
+            command.ExecuteNonQuery();
+
+            Connection.Close();
+
+            return true;
+        }
+
+        public override bool Delete(string query)
+        {
+            Connection.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand(query);
+            command.Connection = Connection;
+
+            command.ExecuteNonQuery();
+
+            Connection.Close();
+
+            return true;
+        }
+    }
+}
