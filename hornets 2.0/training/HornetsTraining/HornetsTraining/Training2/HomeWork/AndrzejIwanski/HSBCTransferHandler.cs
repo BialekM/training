0a0d2ce15b403cfd9e745a11645ffd857940ead6
SAMSUNG550P5C\Andrew <anw93iw@gmail.com>
2016-10-10 22:00:59 +0200

    no message

---
+++ hornets 2.0/training/HornetsTraining/HornetsTraining/Training2/HomeWork/AndrzejIwanski/HSBCTransferHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Toci.HornetsTraining.Training2.HomeWork.AndrzejIwanski
+{
+    public class HSBCTransferHandler : TransferHandler
+    {
+        public override bool DoInTransfer(Transfer transfer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool DoOutTransfer(Transfer transfer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
