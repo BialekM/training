382490411ec264f52c2b9bede148d942aefd9f16
Adrian Pankiewicz <farami11@tlen.pl>
2016-10-14 22:34:32 +0200

    Exception error log with dateTime

---
+++ hornets 2.0/training/HornetsTraining/HornetsTesting/ExceptionHandling/ErrorLog/ErrorLogTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.DesignPatterns.ExceptionHandling;
+using Toci.DesignPatterns.ExceptionHandling.ErrorLog;
+
+
+namespace HornetsTesting.ExceptionHandling.ErrorLog
+{
+    [TestClass]
+    public class ErrorLogTests
+    {
+        [TestMethod]
+        public void ConnectToDatabase()
+        {
+            using (var database = new TociExceptionEntities())
+            {
+                var errorLog = database.ErrorLog;
+            }
+        }
+
+        [TestMethod]
+        public void TociExceptionTest()
+        {
+            int recordsNumber = GetRecordsNumber();
+            try
+            {
+                ThrowTociException();
+            }
+            catch (TociApplicationException)
+            {
+                
+            }
+
+            Assert.AreEqual(GetRecordsNumber(), recordsNumber+1);
+        }
+
+        private int GetRecordsNumber()
+        {
+            using (var database = new TociExceptionEntities())
+            {
+                return database.ErrorLog.Count();
+            }
+        }
+
+        private void ThrowTociException()
+        {
+            throw new TociDbApplicationException("custom message example", 1, "backend message example");
+        }
+
+    }
+}
