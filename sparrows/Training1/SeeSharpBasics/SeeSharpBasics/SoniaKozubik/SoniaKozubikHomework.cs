1809714f13ca55946bb09a3443ad68fc3f790d0d
Sonia <soniakozubik@gmail.com>
2016-10-14 18:24:38 +0200

    homework

---
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/SoniaKozubik/SoniaKozubikHomework.cs
@@ -0,0 +1,32 @@
+﻿using SeeSharpBasics.Training5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharpBasics.SoniaKozubik
+{
+    public class SoniaKozubikHomework:Homework
+    {
+        public override string StringReverse(string candidate)
+        {
+            string result = "";
+            for (int i = candidate.Length - 1; i >= 0; i--)
+            {
+                result += candidate[i];
+            }
+            return result;
+        }
+
+        public override string Substring(string candidate, int start, int length)
+        {
+            string result = "";
+            for (int i = start; i < start + length; i++)
+            {
+                result += candidate[i];
+            }
+            return result;
+        }
+    }
+}
