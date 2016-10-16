d6eaa2fead367d4406a2b115ed6ebd5f83e578b4
DESKTOP-DQOLJU5tkili <g4cester@gmail.com>
2016-10-16 18:50:08 +0200

    zalegly podstawowoy homework

---
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/TomaszKilian/TomaszKilHomework.cs
@@ -0,0 +1,44 @@
+﻿namespace SeeSharpBasics.TomaszKilian
+{
+    public class TomaszKilHomework
+    {
+        public override string StringReverse(string candidate)
+        {
+            string result = "";
+            for (int i = candidate.Length - 1; i >= 0; i--)
+            {
+                result = result + candidate[i];
+            }
+            return result;
+        }
+
+         public override string Substring(string candidate, int start, int length)
+        {
+            string result = "";
+            for (int i = start; i < start + length; i++)
+            {
+                if (start + length > candidate.Length)
+                {
+
+                    break;
+                }
+                else
+                {
+                    result = result + candidate[i];
+
+                }
+
+
+
+            }
+
+            return result;
+
+
+        }
+
+
+
+         
+    }
+}
