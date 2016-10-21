dd165b19c7703f13d4f81dd1366891c6ed7c2624
Sławek Coval <vanter2x@gmail.com>
2016-10-21 22:00:05 +0100

    My List

---
+++ sparrows/Training1/SeeSharpBasics/SeeSharpBasics/SlawekKowal/Training6/SlawekKMyList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace SeeSharpBasics.SlawekKowal.Training6
+{
+    public class SlawekKMyList<T> : IEnumerable
+    {
+        private T[] _list;
+        private int _index = 0;
+
+        public void Add(T adder)
+        {
+            T[] tempList = new T[_index + 1];
+            if (_index > 0) _list.CopyTo(tempList, 0);
+            tempList[_index] = adder;
+            _list = tempList;
+            _index++;
+        }
+
+        public T GetItem(int index)
+        {
+            return _list[index];
+        }
+
+        public void RemoveItem(int index)
+        {
+            T[] tempList = new T[_index - 1];
+            int xCounter = 0;
+            for (int i = 0; i < _index; i++)
+            {
+                if (index != i) tempList[i - xCounter] = GetItem(i);
+                else xCounter += 1;
+            }
+            _list = tempList;
+            _index--;
+        }
+
+        public int GetLenght()
+        {
+            return _index;
+        }
+
+        public int GetIndex(T item)
+        {
+            for (int i = 0; i < _index; i++)
+            {
+                if (_list[i].Equals(item)) return i;
+            }
+            return -1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
---
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/SlawekKowal/Trainig6/TestMySKList.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeSharpBasics.SlawekKowal.Training6;
+using SeeSharpBasics.Training5.Chess;
+
+namespace Toci.SeeSharpBasics.Test.SlawekKowal.Trainig6
+{
+    [TestClass]
+    public class TestMySKList
+    {
+        [TestMethod]
+        public void TestMe()
+        {
+            SlawekKMyList<int> lista = new SlawekKMyList<int>();
+            for (int x = 0; x <= 10; x++) lista.Add(x);
+            var xx = lista.GetLenght();
+            lista.RemoveItem(3);
+            xx = lista.GetIndex(5);
+            xx = lista.GetLenght();
+
+            foreach (var item in lista) Debug.WriteLine(item.ToString());
+
+
+            ChessField[] chField = new ChessField[10];
+            for (int x = 0; x < 10; x++) chField[x] = new ChessField { X = x, Y = 0, Colour = x % 2 == 0 };
+
+            SlawekKMyList<ChessField> lista1 = new SlawekKMyList<ChessField>();
+            foreach (var item in chField) lista1.Add(item);
+            foreach (var item in lista1) Debug.WriteLine(item.ToString());
+        }
+    }
+}
