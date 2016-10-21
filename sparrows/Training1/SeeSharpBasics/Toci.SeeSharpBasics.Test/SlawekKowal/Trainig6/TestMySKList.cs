f30749a7dc6841589d0525196348e3229a4312d0
Sławek Coval <vanter2x@gmail.com>
2016-10-21 22:20:12 +0100

    ach

--- sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/SlawekKowal/Trainig6/TestMySKList.cs
+++ sparrows/Training1/SeeSharpBasics/Toci.SeeSharpBasics.Test/SlawekKowal/Trainig6/TestMySKList.cs
@@ -19,12 +19,11 @@ namespace Toci.SeeSharpBasics.Test.SlawekKowal.Trainig6
             Debug.WriteLine(xx);
             xx = lista.GetLenght();
             Debug.WriteLine(xx);
+
             foreach (var item in lista) Debug.WriteLine(item.ToString());
-
-
             ChessField[] chField = new ChessField[10];
-            for (int x = 0; x < 10; x++) chField[x] = new ChessField { X = x, Y = 0, Colour = x % 2 == 0 };
 
+            for (int x = 0; x < 10; x++) chField[x] = new ChessField { X = x, Y = 0, Colour = x % 2 == 0 };
             SlawekKMyList<ChessField> lista1 = new SlawekKMyList<ChessField>();
             foreach (var item in chField) lista1.Add(item);
             foreach (var item in lista1) Debug.WriteLine(item.ToString());
